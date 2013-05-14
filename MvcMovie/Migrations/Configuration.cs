82d46d7274673a8e3e0ddb5066b0ecd884c8a9d9
Monique <m0_onique@yahoo.com>
2013-05-13 23:01:22 -0400

    Configuration file

--- MvcMovie/Migrations/Configuration.cs
+++ MvcMovie/Migrations/Configuration.cs
@@ -13,45 +13,78 @@ namespace MvcMovie.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
+
         protected override void Seed(MvcMovie.Models.MovieDBContext context)
         {
+
+                context.Genres.AddOrUpdate(i => i.Name,
+                   new Genre
+                   {
+                       Name = "Action"
+                   }
+               );
+                
+                context.Ratings.AddOrUpdate(i => i.Name,
+                    new Rating { 
+                        Name = "G"
+                    },
+
+                    new Rating { 
+                        Name = "PG"
+                    },
+
+                    new Rating { 
+                        Name = "PG-13"
+                    },
+
+                    new Rating { 
+                        Name = "R"
+                    },
+
+                    new Rating { 
+                        Name = "NC-17"
+                    }
+
+                );
+
                 context.Movies.AddOrUpdate(i => i.Title,
-            new Movie
-            {
-                Title = "When Harry Met Sally",
-                ReleaseDate = DateTime.Parse("1989-1-11"),
-                Genre = "Romantic Comedy",
-                Price = 7.99M,
-                Rating = "G"
-            },
+                    new Movie
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-1-11"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M,
+                        Rating = "G"
+                    },
 
-             new Movie
-             {
-                 Title = "Ghostbusters ",
-                 ReleaseDate = DateTime.Parse("1984-3-13"),
-                 Genre = "Comedy",
-                 Price = 8.99M,
-                 Rating = "G"
-             },
+                     new Movie
+                     {
+                         Title = "Ghostbusters ",
+                         ReleaseDate = DateTime.Parse("1984-3-13"),
+                         Genre = "Comedy",
+                         Price = 8.99M,
+                         Rating = "G"
+                     },
 
-             new Movie
-             {
-                 Title = "Ghostbusters 2",
-                 ReleaseDate = DateTime.Parse("1986-2-23"),
-                 Genre = "Comedy",
-                 Price = 9.99M,
-                 Rating = "G"
-             },
+                     new Movie
+                     {
+                         Title = "Ghostbusters 2",
+                         ReleaseDate = DateTime.Parse("1986-2-23"),
+                         Genre = "Comedy",
+                         Price = 9.99M,
+                         Rating = "G"
+                     },
 
-           new Movie
-           {
-               Title = "Rio Bravo",
-               ReleaseDate = DateTime.Parse("1959-4-15"),
-               Genre = "Western",
-               Price = 3.99M,
-               Rating = "G"
-           }
-       );
+                   new Movie
+                   {
+                       Title = "Rio Bravo",
+                       ReleaseDate = DateTime.Parse("1959-4-15"),
+                       Genre = "Western",
+                       Price = 3.99M,
+                       Rating = "G"
+                   }
+               );
+
 
         }
     }
